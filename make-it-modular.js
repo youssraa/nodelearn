5f1fe8243bb6a8696048a5fbe31177b42dba7fd5
youssraa <tayachii.yosra@gmail.com>
2021-01-21 04:31:54 +0100

    learnnode

---
+++ make-it-modular.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function(directory, fileExtension, callback) {
+    fs.readdir(directory, (error, fileList) => {
+        if(error) {
+            return callback(error);
+        }
+
+        const filteredFiles = fileList.filter((file) => {
+            return path.extname(file) === '.' + fileExtension;
+        });
+
+        callback(null, filteredFiles);
+    });
+};
